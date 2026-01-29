dd247ab7244be92f5f2d8cc3c2958e3ec2aa89e7
sudan94 <sudanupadhaya@gmail.com>
2026-01-29 20:11:17 +0100

    Fixed routing issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -52,7 +52,6 @@ function App() {
                 </ProtectedRoute>
               }
             />
-            <Route path="/" element={<Navigate to="/dashboard" replace />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </Router>
